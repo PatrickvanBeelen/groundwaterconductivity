63d5b6fa90faad70ea83725861d99d0a61ebd3cb
beelenvp <patrick.van.beelen@rivm.nl>
2022-07-26 12:48:57 +0200

    probeer namespace error weg te krijgen

--- R/LoadFileInVariable.R
+++ R/LoadFileInVariable.R
@@ -1,7 +1,6 @@
 #' Loads an .rda data file into a variable
 #' myTable<-LoadFileInVariable("data/Table.rda")
 #' @param filename Input datafile like data/Table.rda
-#' @export
 LoadFileInVariable <- function(filename) {
   load(filename)
   get(ls()[ls() != "filename"])
